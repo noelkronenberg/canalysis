ecd5240b4bc584a791c5ea7eabf770bf165f3e9e
Noel Kronenberg <79874249+noelkronenberg@users.noreply.github.com>
2024-03-12 18:14:50 +0000

    added option for locations

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ import datetime
 
 app = Flask(__name__)
 
-def calculate_hours(files, titles, time_frame):
+def get_data(files, titles, time_frame, location_status):
     total_hours = {title: 0 for title in titles} # placeholder dictionary
     # format start and end date
     start_date = datetime.datetime.strptime(time_frame[0], '%Y-%m-%d').date() # reference: https://stackoverflow.com/a/46468667
     end_date = datetime.datetime.strptime(time_frame[1], '%Y-%m-%d').date() + datetime.timedelta(days=1) # add one day to include end date (reference: https://stackoverflow.com/a/3240493)
-     # iterate through all files
+    locations = set()
+    # iterate through all files
     for file in files:
         ical_data = file.read()
         # iterate through all events
@@ -22,7 +23,14 @@ def calculate_hours(files, titles, time_frame):
                     start_time = event.get('DTSTART').dt
                     end_time = event.get('DTEND').dt
                     total_hours[title] += (end_time - start_time).total_seconds() / 3600 # calculate and assign duration to title
-    return total_hours
+                    
+                    # save location
+                    if location_status:
+                        location = event.get('LOCATION')
+                        if location != None:
+                            locations.add(location)
+                            
+    return total_hours, list(locations)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -31,10 +39,11 @@ def index():
         titles = [item.strip() for item in request.form.get('titles').split(',')]
         from_date = request.form['from']
         to_date = request.form['to']
-        total_hours = calculate_hours(files, titles, (from_date, to_date))
+        location_status = request.form.get('location') == 'on'
+        total_hours, locations = get_data(files, titles, (from_date, to_date), location_status)
         rounded_hours = {k: round(v,2) for k, v in total_hours.items()}
         sorted_hours = dict(sorted(rounded_hours.items(), key=lambda item: item[1], reverse=True))
-        return render_template('result.html', data=sorted_hours)
+        return render_template('result.html', data=sorted_hours, locations=locations)
     return render_template('index.html')
 
 if __name__ == '__main__':
